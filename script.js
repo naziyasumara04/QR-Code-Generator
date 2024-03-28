745734c88f7b684291559d46638743dcef735c28
naziyasumara04 <girlofficial297@gmail.com>
2024-03-28 11:44:13 -0700

    edited

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
-let btn=document.querySelector("#btn");
-let qrCode=document.querySelector("#qrCode");
-let img=document.querySelector("#code-img");
+let btn = document.querySelector("#btn");
+let qrCode = document.querySelector("#qrCode");
+let img = document.querySelector("#code-img");
 
-btn.addEventListener("click",()=>{
-    // console.log("btnclicked");
-    let input=document.querySelector("#input");
-    console.log(input.value);
+// btn.addEventListener("click", (event) => {
+//     event.preventDefault(); 
+//     let input = document.querySelector("#input");
+    
+//     let api=`https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${input.value}`;
+    
+//     img.src=api;
+// });
+
+
+btn.addEventListener("click", (event) => {
+    let input = document.querySelector("#input");
+    event.preventDefault();
+    if(input.value==" "){
+        img.style.display="none";
+    }else{
+     // Prevent default form submission behavior
+    
+    // console.log(input.value);
+    let api=`https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${input.value}`;
+    // console.log(api);
+    img.src=api;
+    }
 });
