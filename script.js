02a226d65331f31fe8600187927ce733f5cb51dd
naziyasumara04 <girlofficial297@gmail.com>
2024-03-28 10:55:30 -0700

    initial

---
+++ script.js
@@ -0,0 +1,9 @@
+let btn=document.querySelector("#btn");
+let qrCode=document.querySelector("#qrCode");
+let img=document.querySelector("#code-img");
+
+btn.addEventListener("click",()=>{
+    // console.log("btnclicked");
+    let input=document.querySelector("#input");
+    console.log(input.value);
+});
