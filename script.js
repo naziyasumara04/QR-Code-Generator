dfd3e86bc7b7becef8f3a72f912d2955c0b9d886
naziyasumara04 <girlofficial297@gmail.com>
2024-03-29 01:02:31 -0700

    updated

--- script.js
+++ script.js
@@ -5,9 +5,9 @@ let img = document.querySelector("#code-img");
 // btn.addEventListener("click", (event) => {
 //     event.preventDefault(); 
 //     let input = document.querySelector("#input");
-    
+
 //     let api=`https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${input.value}`;
-    
+
 //     img.src=api;
 // });
 
@@ -15,14 +15,15 @@ let img = document.querySelector("#code-img");
 btn.addEventListener("click", (event) => {
     let input = document.querySelector("#input");
     event.preventDefault();
-    if(input.value==" "){
-        img.style.display="none";
-    }else{
-     // Prevent default form submission behavior
-    
-    // console.log(input.value);
-    let api=`https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${input.value}`;
-    // console.log(api);
-    img.src=api;
+    if (input.value == " ") {
+        img.style.display = "none";
+    } else {
+        // Prevent default form submission behavior
+
+        // console.log(input.value);
+        let api = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${input.value}`;
+        // console.log(api);
+        img.src = api;
+        // img.style.display = "block";
     }
 });
